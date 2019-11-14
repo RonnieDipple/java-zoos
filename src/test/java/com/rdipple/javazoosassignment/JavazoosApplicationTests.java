fc5058dd6cf7b4efebb956dadd96b7fb174a9ced
RonnieDipple <Ronniedipple@gmail.com>
2019-11-13 20:40:23 -0600

    Problems with folders and beans fixed working

---
+++ src/test/java/com/rdipple/javazoosassignment/JavazoosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rdipple.javazoosassignment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavazoosApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
